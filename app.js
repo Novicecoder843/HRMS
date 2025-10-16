c9800435d946974f60ef1d447d2f339eeb88d77c
ajitCode <ajitmohapatra1024@gmail.com>
2025-10-16 18:09:40 +0530

    Revert "HRMS"
    This reverts commit fd1ea370b2db91eab2bbffbb0e3c114b7e52ebe7.

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-// const  express = require("express");
+const  express = require("express");
 // import dotenv from "dotenv";
-// import routes from "./routes/index.js";
+import routes from "./routes/index.js";
 // import sequelize from "./config/db.config.js";
 // import errorMiddleware from "./middleware/error.middleware.js";
 
 // dotenv.config();
 
-// const app = express();
+const app = express();
 //middelware -- 
-// app.use(express.json());
+app.use(express.json());
 // http://localhost:4000/api/v1/user/adduser
 // http://localhost:4000/api/v1/user/deleteuser
 // http://localhost:4000/api/v1/user/updateuser
@@ -16,46 +16,10 @@
 // http://localhost:4000/api/v1/attandnce/getattend
 // http://localhost:4000/api/v1/attandnce/insertattendcne
 
-// app.use("/api/v1", routes);
-// app.use(errorMiddleware);
-
-// sequelize.sync({ alter: false }).then(() => console.log("Database connected ✅"));
-
-// const PORT = process.env.PORT || 4000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-// const sequelize = require("./src/config/db.config");
-
-// sequelize.sync({ alter: true })
-//   .then(() => console.log("✅ Models synced with database"))
-//   .catch((err) => console.log("❌ Error syncing models:", err));
-
-
-
-
-
-
-const express = require("express");
-const routes = require("./routes/index");
-const sequelize = require("./config/db.config");
-const errorMiddleware = require("./middlewares/error.middleware");
-
-const app = express();
-
-// Middleware
-app.use(express.json());
-
-// Routes
 app.use("/api/v1", routes);
-
-// Error middleware
 app.use(errorMiddleware);
 
-// Sequelize DB connection
-sequelize
-  .sync({ alter: true })
-  .then(() => console.log("✅ Database connected and models synced"))
-  .catch((err) => console.error("❌ Database connection error:", err));
+sequelize.sync({ alter: false }).then(() => console.log("Database connected ✅"));
 
-// Server start
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
