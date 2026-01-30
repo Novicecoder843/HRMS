48061d9f26cbb6ef76398dfcea57e46fd17463da
Asish <asishbehera524@gmail.com>
2026-01-30 16:02:58 +0530

    users curd

---
+++ routes/designation.routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const designationController = require("../controllers/designation.controller");
+
+router.post("/", designationController.createDesignation);
+router.get("/", designationController.getAllDesignations);
+router.get("/:id", designationController.getDesignationById);
+router.put("/:id", designationController.updateDesignation);
+router.delete("/:id", designationController.deleteDesignation);
+
+module.exports = router;
