a28be5bf6e6b82ecb276ee306d9032a41a6c5ca8
rakeshkumarsahoo0108-svg <rakeshkumarsahoo0108@gmail.com>
2026-03-13 12:49:43 +0530

    updated

--- Routes/company.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const CompanyController = require("../Controllers/company.controller");
-
-router.post("/signup", CompanyController.signup);
-router.post("/login", CompanyController.login);
-
-module.exports = router;
